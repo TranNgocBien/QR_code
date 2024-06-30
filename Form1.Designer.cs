96b22429baf74667ed95e1585107b2f952b44804
TranNgocBien <tranngocbien2001@gmail.com>
2024-06-30 23:29:32 +0700

    QRcode 30.6.2024

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Qrcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbCamera = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txbCode = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridViewList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(474, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Get QR code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select Input Camera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(76, 484);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Readed Code";
+            // 
+            // cbCamera
+            // 
+            this.cbCamera.FormattingEnabled = true;
+            this.cbCamera.Location = new System.Drawing.Point(370, 142);
+            this.cbCamera.Name = "cbCamera";
+            this.cbCamera.Size = new System.Drawing.Size(332, 24);
+            this.cbCamera.TabIndex = 3;
+            this.cbCamera.SelectedIndexChanged += new System.EventHandler(this.cbCamera_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(67, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(67, 364);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 47);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(246, 219);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 204);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txbCode
+            // 
+            this.txbCode.Location = new System.Drawing.Point(287, 472);
+            this.txbCode.Multiline = true;
+            this.txbCode.Name = "txbCode";
+            this.txbCode.Size = new System.Drawing.Size(308, 44);
+            this.txbCode.TabIndex = 7;
+            this.txbCode.TextChanged += new System.EventHandler(this.txbCode_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(325, 560);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "GET CODE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridViewList
+            // 
+            this.dataGridViewList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewList.Location = new System.Drawing.Point(601, 219);
+            this.dataGridViewList.Name = "dataGridViewList";
+            this.dataGridViewList.RowHeadersWidth = 51;
+            this.dataGridViewList.RowTemplate.Height = 24;
+            this.dataGridViewList.Size = new System.Drawing.Size(747, 297);
+            this.dataGridViewList.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1360, 638);
+            this.Controls.Add(this.dataGridViewList);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txbCode);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbCamera);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbCamera;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txbCode;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridViewList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+
+namespace Qrcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridViewList.ColumnCount = 5;
+            dataGridViewList.Columns[0].Name = "ID";
+            dataGridViewList.Columns[1].Name = "MachineName";
+            dataGridViewList.Columns[2].Name = "Operator";
+            dataGridViewList.Columns[3].Name = "StartTime";
+            dataGridViewList.Columns[4].Name = "EndTime";
+
+            dataGridViewList.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewList.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewList.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewList.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewList.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+
+        }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+     
+
+
+        //start button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cbCamera.SelectedIndex].MonikerString);
+
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //throw new NotImplementedException();
+            //
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            BarcodeReader reader = new BarcodeReader(); // tao doi tuong doc barcode
+            var result = reader.Decode(bitmap); //doc qr code
+            if (result != null)
+            {
+                txbCode.Invoke(new MethodInvoker(delegate ()
+                {
+                    txbCode.Text = result.ToString(); // hien thi ket qua doc duoc len textbox
+
+                }
+                ));
+
+            }
+            pictureBox1.Image = bitmap; // hien thi frame hinh anh len tren pictureBox
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice != null)
+            {
+                if (videoCaptureDevice.IsRunning)
+                {
+                    videoCaptureDevice.Stop();
+                    pictureBox1.Image = null;
+                }
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (videoCaptureDevice != null)
+            {
+                if (videoCaptureDevice.IsRunning)
+                {
+                    videoCaptureDevice.Stop();
+                    pictureBox1.Image = null;
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //lay ds tat ca cac thiet bi video dang dc ket noi
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            //them ten cua moi thiet bi vao dropdow cua combobox
+            foreach (FilterInfo device in filterInfoCollection)
+                cbCamera.Items.Add(device.Name);
+            cbCamera.SelectedIndex = 0;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Lấy nội dung từ TextBox1
+            string inputText = txbCode.Text;
+
+            // Tách chuỗi đầu vào thành các phần
+            string[] parts = inputText.Split(',');
+            if (parts.Length != 5)
+            {
+                MessageBox.Show("Dữ liệu đầu vào không hợp lệ!");
+                return;
+            }
+
+            // Xử lý dữ liệu
+            string id = parts[0].Trim();
+            string machine = parts[1].Trim();
+            string name = parts[2].Trim();
+            string start = parts[3].Trim();
+            string end = parts[4].Trim();
+
+
+            int yearS = int.Parse(start.Substring(0, 4));
+            int monthS = int.Parse(start.Substring(4, 2));
+            int dayS = int.Parse(start.Substring(6, 2));
+            int hourS = int.Parse(start.Substring(8, 2));
+            int minuteS = int.Parse(start.Substring(10, 2));
+            int secondS = int.Parse(start.Substring(12, 2));
+
+            DateTime datetimeStart = new DateTime(yearS, monthS, dayS, hourS, minuteS, secondS);
+            string datetimeStartString = datetimeStart.ToString("dd/MM/yyyy HH:mm:ss");
+
+
+
+            int yearE = int.Parse(end.Substring(0, 4));
+            int monthE = int.Parse(end.Substring(4, 2));
+            int dayE = int.Parse(end.Substring(6, 2));
+            int hourE = int.Parse(end.Substring(8, 2));
+            int minuteE = int.Parse(end.Substring(10, 2));
+            int secondE = int.Parse(end.Substring(12, 2));
+
+            DateTime datetimeEnd = new DateTime(yearE, monthE, dayE, hourE, minuteE, secondE);
+            string datetimeEndString = datetimeEnd.ToString("dd/MM/yyyy HH:mm:ss");
+
+         
+
+
+
+            // Tạo chuỗi kết quả
+            //string result = $"ID: {id}\nMáy: {machine}\nTên: {name}";
+
+            // Hiển thị kết quả trong TextBox2
+            //display.Text = result;
+
+            //
+            //addRow(id, machine, name, datetimeStartString, datetimeEndString);
+            addRow(id, machine, name, datetimeStartString, datetimeEndString);
+
+
+        }
+
+        private void addRow(string id, string machine, string name, string datetimeStartString, string datetimeEndString)
+        {
+            String[] row = { id, machine, name, datetimeStartString, datetimeEndString };
+            dataGridViewList.Rows.Add(row);   
+
+        }
+
+        private void cbCamera_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbCode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
